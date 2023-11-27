343dd64fe0072425bbd16bec222df1009b90ba44
Brendan Dempsey <bjd1412@gmail.com>
2023-11-27 03:14:27 -0500

    completed lab

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log("HELLO")
+}
+
+function logWhisper(string) {
+    console.log("hello");
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    const dinner = "I would love to!"
+    const hello = "I can't hear you!"
+    const HELLO = "YES INDEED!"
+    if (string.toLowerCase(string) === string) {
+        return hello;
+    }
+    else if (string.toUpperCase(string) === string) {
+        return HELLO;
+    }
+    else if ("Let's have dinner together!" === string) {
+        return dinner
+    }
+
+    
+}
